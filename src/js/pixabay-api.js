2d0dc11f0548078644798bb440be6ff9ff46eafb
Yehor <egorberezenko7@gmail.com>
2025-06-04 10:04:08 +0200

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '50573196-0c5162e61d19a838e32427977';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
---
+++ src/main.js
@@ -0,0 +1,56 @@
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.elements['search-text'];
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query.',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    hideLoader();
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    hideLoader();
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topRight',
+    });
+    console.error(error);
+  }
+  form.reset();
+});
